517d3e38b7acc120f709141d98ad24b058dcf080
MuraliB91 <muralib91@hotmail.com>
2020-07-19 12:32:32 -0700

    Comments: ICommand inherited class - Immutable/read-only properties

--- DNC-DShop.Services.Discounts/src/DShop.Services.Discounts/Messages/Commands/CreateDiscount.cs
+++ DNC-DShop.Services.Discounts/src/DShop.Services.Discounts/Messages/Commands/CreateDiscount.cs
@@ -8,11 +8,14 @@ namespace DShop.Services.Discounts.Messages.Commands
     // Custom routing key: #.discounts.create_discount
     public class CreateDiscount : ICommand
     {
+        //NOTE: all are Read-Only properties/Immutable properties. Because dont need to change incoming cmd
+        //Also all the msgs posted to the SvcBus must be immutable as well
         public Guid Id { get; }
         public Guid CustomerId { get; }
         public string Code { get; }
         public double Percentage { get; }
 
+        //To deserialize incoming messages
         [JsonConstructor]
         public CreateDiscount(Guid id, Guid customerId,
             string code, double percentage)
